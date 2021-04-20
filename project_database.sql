5a18fa54a4dfef0430d5e30b91eed1b0ba475a0e
sayedsafwe77 <sayedsafwet4@gmail.com>
2021-04-20 20:45:48 +0200

    update sql file

--- project_database.sql
+++ project_database.sql
@@ -1,21 +1,22 @@
-create database php_project
+create database php_project;
+use php_project;
 create TABLE admin(
 id INT(10) PRIMARY KEY AUTO_INCREMENT,
-username VARCHAR(50) ,
+username VARCHAR(50),
 email VARCHAR(100),
 userpassword VARCHAR(100),
 image VARCHAR(100),
 role enum('user','admin')
-)
+);
 
 create TABLE product(
 id INT(10) PRIMARY KEY AUTO_INCREMENT,
-productname VARCHAR(50) ,
+productname VARCHAR(50),
 category_id INT(10)
-)
+);
 
 create TABLE category(
 id INT(10) PRIMARY KEY AUTO_INCREMENT,
-categorytname VARCHAR(50)
-)
+categoryname VARCHAR(50)
+);
 ALTER TABLE product ADD FOREIGN KEY (category_id) REFERENCES category(id);
